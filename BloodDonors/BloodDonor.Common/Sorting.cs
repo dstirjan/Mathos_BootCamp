02d7de61391616e9c4aa351f7ecd4e94f1770728
dstirjan <stirjan.dejan@gmail.com>
2022-04-06 09:19:12 +0200

    test

--- BloodDonors/BloodDonor.Common/Sorting.cs
+++ BloodDonors/BloodDonor.Common/Sorting.cs
@@ -8,7 +8,7 @@ namespace BloodDonor.Common
 {
     public class Sorting
     {
-       public string SortOrder { set; get; }
-        public string SortBy { set; get; } = "ASC";
+       public string SortOrder { set; get; } = "ASC";
+        public string SortBy { set; get; } 
     }
 }
